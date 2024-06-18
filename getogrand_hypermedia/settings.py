86301dc15494f116f08257494a5274d348bdbeac
Wonyoung Ju <getogrand@hey.com>
2024-06-18 18:29:09 +0900

    whitenoise 적용

--- getogrand_hypermedia/settings.py
+++ getogrand_hypermedia/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     "main.middlewares.HealthCheckMiddleware",
     "main.middlewares.DisableAdminI18nMiddleware",
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -157,6 +158,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "www/static/"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
